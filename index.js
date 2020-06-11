345c0eb9c710a9215e0f51ac0192db9e2ff6098b
Alva Cardona <a_cardonac@hotmail.com>
2020-06-11 02:06:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // write your code below!
 var name = "Susan"
 var height = 39
-var message = `${name} is ${height} inches tall`
+var message = `${height} is ${height} inches tall`
 
 module.exports = {name, height, message}
+
+console.log(message)
